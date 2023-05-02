6695373a0f7773694b6ef589cc2a54e128d53b6d
erine_n <enatnat.08@gmail.com>
2023-05-02 14:39:03 -0700

    advice-generator completed

---
+++ advice-generator-app-main/completed/README.md
@@ -0,0 +1,62 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Upon entering the site see a loading screen for a clean user experience
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Screenshot
+
+![Screengrab](ezgif.com-video-to-gif.gif)
+
+### Links
+
+- [Live Site](https://6451828d06603a1f3fb91ef4--precious-dusk-9e7856.netlify.app/)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [Vue3](https://vuejs.org/) - JS library
+- npm package: [VueJS Spinner](https://www.npmjs.com/package/vue-spinner)
+
+
+### What I learned
+
+This was great practice to get me more acquainted with VueJS and working with APIs. It was simple and I would like to try more challenging APIs. 
+
+
+## Author
+
+- Website - [Erine Natnat](https://www.linkedin.com/in/erinenatnat)
+- Frontend Mentor - [@rinster](https://www.frontendmentor.io/profile/rinster)
+- Twitter - [@rinster](https://www.twitter.com/rinster)
+
+
+
--- advice-generator-app-main/completed/advice-generator/src/App.vue
+++ advice-generator-app-main/completed/advice-generator/src/App.vue
@@ -1,13 +1,155 @@
 <template>
-  <p>Hello World</p>
+  <main>  
+    <ring-loader :loading="loading" :color="color" :size="size"></ring-loader>
+    <div class="adviceCard" v-if="loading === false">
+      <p>Advice # {{ currentQuoteId }}</p>
+       <h1>“{{ currrentQuote }}”</h1>
+       <div class="cardBotton">
+        <svg class="desktop" width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+        <svg class="mobile" width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+        <button @click="fetchAdvice"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg></button>
+      </div>
+    </div>
+    </main>
 </template>
 
 <script>
+import RingLoader from 'vue-spinner/src/RingLoader.vue';
 export default {
   name: 'App',
+  components: {
+    RingLoader
+  },
+  data() {
+    return {
+      loading: true,
+      currrentQuote : "",
+      currentQuoteId: null,
+      size: 700,
+      color: "#4DDD99"
+    }
+  },
+  async created() {
+    setTimeout(() => {
+       this.fetchAdvice()
+    }, 2000)
+  },
+  methods: {
+    fetchAdvice: async function() {
+      const resp = await fetch("https://api.adviceslip.com/advice");
+      const data = await resp.json();
+      this.currrentQuote = data.slip.advice;
+      this.currentQuoteId = data.slip.id;
+      this.loading = false;
+    }
+  }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #202732;
+  font-size: 18px;
+  font-family: Manrope, sans-serif;
+  font-weight: 800;
+}
+main {
+  display: flex;
+  justify-content: center;
+  position: relative;
+  min-height: 100vh;
+}
+.v-spinner {
+  position: absolute;
+  top: 44%;
+  left: 45%;
+  transform: translate(-50%, -50%);
+}
+.adviceCard {
+  position: relative;
+  background-color: #303A48;
+  color: #CEE2E9;;
+  padding: 50px 50px 70px 50px;
+  border-radius: 20px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+  max-height: 500px;
+  max-width: 600px;
+  text-align: center;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+}
+.adviceCard p {
+  color: #4DDD99;
+  text-transform: uppercase;
+  letter-spacing: .25em;
+  font-size: .9em;
+  font-weight: 400;
+  margin-bottom: 2rem;
+}
+.cardBottom {
+  width: 100%;
+  margin-bottom: 1.5rem;
+}
+h1 {
+    margin-bottom: 2.5rem;
+    font-weight: 400;
+}
+
+button {
+  background-color: hsl(150, 100%, 66%);
+  border-radius: 100%;
+  padding: 20px;
+  border: hsl(150, 100%, 66%);
+  position: absolute;
+  bottom: -40px;
+  left: 45%;
+  transition: all 0.2s ease-in-out;
+
+}
+@media(hover: hover) {
+  button:hover {
+    color: hsl(150, 100%, 66%);
+    box-shadow: 0px 0px 26px rgba(82, 255, 168, .9);
+    padding: 20px;
+  }
+}
+/**Remove hover on mobile screens*/
+@media(hover: none) {
+  button:hover, button:active {border: 1px solid rgba(82, 255, 168, .9)}
+}
+
+@media screen and (min-width: 375px) {
+  .mobile {
+    display: none;
+  }
+}
+@media screen and (max-width: 374px) {
+  .desktop {
+    display: none;
+  }
+  .mobile {
+    display: block;
+  }
+  .adviceCard {
+    max-width: 350px;
+    padding: 30px 30px 50px 30px;
+  }
+  p {
+    font-size: .6em;
+  }
+  h1 {
+    font-size: 24px;
+  }
+  button {
+    left: 42%;
+  }
+}
 </style>
